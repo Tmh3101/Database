cb8aafd33b8a42b4f3034f469b530e96c62c0ed8
Trần Minh Hiểu <131644516+Tmh3101@users.noreply.github.com>
2024-04-12 12:30:44 +0700

    Update TP6_Pizza.sql

--- TP6_Pizza.sql
+++ TP6_Pizza.sql
@@ -77,3 +77,100 @@ insert into PHUC_VU values('New York Pizza', 'supreme', 8.5);
 insert into PHUC_VU values('Chicago Pizza', 'cheese', 7.75);
 insert into PHUC_VU values('Chicago Pizza', 'supreme', 8.5);
 
+-- 1. - Thêm khóa chính cho các bảng đã cho
+--    - Đặt thuộc tính "ten" là khóa ngoại của bảng LUI_TOI và AN
+--    - THêm ràng buộc gia>0 cho cột "gia" của bảng PHUC_VU
+
+ALTER TABLE NGUOI_AN
+ADD CONSTRAINT pk_nguoian_ten PRIMARY KEY (TEN)
+
+ALTER TABLE LUI_TOI
+ADD CONSTRAINT fk_luitoi_ten FOREIGN KEY (TEN) REFERENCES NGUOI_AN (TEN)
+  
+ALTER TABLE LUI_TOI
+ADD CONSTRAINT pk_luitoi_ten_quanPizza PRIMARY KEY (TEN, QUANPIZZA)
+
+ALTER TABLE AN
+ADD CONSTRAINT fk_an_ten FOREIGN KEY (TEN) REFERENCES NGUOI_AN (TEN)
+  
+ALTER TABLE AN
+ADD CONSTRAINT pk_an_ten_pizza PRIMARY KEY (TEN, PIZZA)
+  
+ALTER TABLE PHUC_VU
+ADD CONSTRAINT pk_phucvu_quanPizza_pizza_gia PRIMARY KEY (QUANPIZZA, PIZZA, GIA)
+
+-- 2. Cho biết quán 'Pizza Hut' đã phục vụ các bánh pizza nào
+SELECT PIZZA FROM PHUC_VU
+WHERE QUANPIZZA = 'Pizza Hut';
+
+-- 3. Danh sách các bánh pizza mà các quán có bán
+SELECT DISTINCT PIZZA FROM PHUC_VU;
+
+-- 4. Cho biết tên các quán có phục vụ các bánh pizza mà tên gồm chữ 'm'
+SELECT DISTINCT QUANPIZZA FROM PHUC_VU
+WHERE PIZZA LIKE ('%m%');
+
+-- 5. Tìm tên và tuổi của người ăn đã đến quán 'Dominos', sắp xếp kết quả giảm dần theo tuổi
+SELECT NA.TEN, TUOI FROM LUI_TOI LT
+JOIN NGUOI_AN NA ON LT.TEN = NA.TEN
+WHERE QUANPIZZA = 'Dominos'
+ORDER BY TUOI DESC;
+
+-- 6. Tìm tên quán pizza và số bánh pizza mà mỗi quán phục vụ
+SELECT QUANPIZZA, COUNT(PIZZA) SB FROM PHUC_VU
+GROUP BY QUANPIZZA;
+
+-- 7. Tìm tên những quán pizza phục cụ pizza với giá cao nhất
+SELECT DISTINCT QUANPIZZA FROM PHUC_VU
+WHERE GIA >= ALL (
+    SELECT GIA FROM PHUC_VU
+);
+
+-- 8. Tìm tên các quán phục vụ ít nhất một bánh pizza mà "Ian" thích ăn
+SELECT DISTINCT QUANPIZZA FROM PHUC_VU
+WHERE PIZZA IN (
+    SELECT PIZZA FROM AN
+    WHERE TEN = 'Ian'
+);
+
+-- 9. Tìm tên các quán có phục vụ ít nhất một bánh mà "Eli" không thích
+SELECT DISTINCT QUANPIZZA FROM PHUC_VU
+WHERE PIZZA NOT IN (
+    SELECT PIZZA FROM AN
+    WHERE TEN = 'Eli'
+);
+
+-- 10. Tìm tên các quán chỉ phục vụ các bánh mà "Eli" thích (chua xong)
+SELECT QUANPIZZA FROM PHUC_VU
+WHERE QUANPIZZA IN (
+    SELECT DISTINCT QUANPIZZA FROM PHUC_VU
+    WHERE PIZZA IN (
+        SELECT PIZZA FROM AN
+        WHERE TEN = 'Eli'
+    )
+)
+GROUP BY QUANPIZZA
+HAVING COUNT(PIZZA) = (
+    SELECT COUNT(PIZZA) FROM AN
+    WHERE TEN = 'Eli'
+    GROUP BY TEN
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
